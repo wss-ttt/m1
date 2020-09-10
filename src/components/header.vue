4e7a72bc08654d6ee1ff36fbbcf0bae8b6b3ef61
王水胜 <602466011@qq.com>
2020-09-10 18:05:33 +0800

    添加: header.veu组件

---
+++ src/components/header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <mt-header :title="pageTitle">
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+    </mt-header>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['pageTitle'])
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
+</script>
+<style scoped>
+</style>
