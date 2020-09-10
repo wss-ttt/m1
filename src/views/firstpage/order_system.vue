3e039fd55088904c7db925d769d08ead5cdb2014
王水胜 <602466011@qq.com>
2020-09-10 17:24:39 +0800

    添加: 添加页面

---
+++ src/views/firstpage/order_system.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    订单系统
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
+</script>
+<style scoped lang="scss">
+.wrapper {
+}
+</style>
