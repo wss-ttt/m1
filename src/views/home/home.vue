af9e7320a02c05c3cf22feaf1d28112205da0823
王水胜 <602466011@qq.com>
2020-09-10 14:57:27 +0800

    添加: 页面

---
+++ src/views/home/home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <div class="header">艾如是</div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <v-tab></v-tab>
+  </div>
+</template>
+
+<script>
+import VTab from '@/components/tab.vue'
+export default {
+  components: {
+    VTab
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
+</script>
+<style scoped lang="scss">
+@mixin center () {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.wrapper {
+  .header {
+    height: 40px;
+    background: #ccc;
+    @include center();
+  }
+}
+</style>
