267d3b4ee7bd959d2c8c2a3588395b4574eb323b
王水胜 <602466011@qq.com>
2020-09-11 14:12:09 +0800

    添加: person相关页面

---
+++ src/views/person/showidentity.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <v-header />
+    <div class="content">身份证正反面</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+    this.$store.commit('common/updatePageTitle', this.$route.meta.title)
+    // 还需要保存上级路由地址到store中去
+    this.$store.commit('common/updateHeaderParentPath', '/home/personal')
+  },
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
+</script>
+<style scoped>
+</style>
