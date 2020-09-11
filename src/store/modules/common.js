61edbc613f2352bc02fdc56f97fb73388b8b7f18
王水胜 <602466011@qq.com>
2020-09-11 08:34:45 +0800

    更新: common.js

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,37 +1,17 @@
-import request from '@/utils/request.js'
 export default {
-    namespaced: false,   // 命名空间
-    state: {
-        users: [],  // 保存用户数据
-        start: 0,   // 开始的位置
+  namespaced: true, // 命名空间
+  state: {
+    pageTitle: "", // 页面标题
+    headerParentPath: "" // 保存上级路由地址
+  },
+  getters: {},
+  actions: {},
+  mutations: {
+    updatePageTitle(state, pageTitle) {
+      state.pageTitle = pageTitle;
     },
-    getters: {
-
-    },
-    actions: {
-        getUsers({ commit, state },payload) {
-            request({
-                url: 'http://localhost:3000/users/list',
-                method: 'get',
-                params: {
-                    start: state.start,
-                    end: state.start + 3
-                }
-            }).then((data) => {
-                commit('changeStart')
-                commit({
-                    type: 'getUsers',
-                    res: data
-                })
-            })
-        },
-    },
-    mutations: {
-        changeStart(state, payload) {
-            state.start += 3
-        },
-        getUsers(state, payload) {
-            state.users = state.users.concat(payload.res)
-        },
+    updateHeaderParentPath(state, headerParentPath) {
+      state.headerParentPath = headerParentPath;
     }
-}
+  }
+};
