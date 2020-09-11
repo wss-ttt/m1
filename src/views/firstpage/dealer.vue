bfd8675176ee4deab7771496a2cd7342871344cb
王水胜 <602466011@qq.com>
2020-09-11 11:39:26 +0800

    更新: pageTitle 和 headerParentPath

--- src/views/firstpage/dealer.vue
+++ src/views/firstpage/dealer.vue
@@ -1,29 +1,32 @@
 <template>
-    <div class="wrapper">
-      经销商 
-   </div>
+  <div class="wrapper">
+    <v-header />
+    我的经销商
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    props: {},
-    data() {
-      return {
-          
-      }
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    activated() {},
-    deactivated() {},
-    updated() {},
-    destroyed() {},
-    methods: {},
-    filters: {}
-  }
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+    this.$store.commit('common/updatePageTitle', '我的经销商')
+    // 还需要保存上级路由地址到store中去
+    this.$store.commit('common/updateHeaderParentPath', '/home/firstpage')
+  },
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
 </script>
 <style scoped>
 </style>
--- src/views/firstpage/order_system.vue
+++ src/views/firstpage/order_system.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="wrapper">
+    <v-header />
     订单系统
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
+  components: {
+  },
   props: {},
   data() {
     return {}
@@ -14,7 +16,11 @@ export default {
   computed: {},
   watch: {},
   created() {},
-  mounted() {},
+  mounted() {
+    this.$store.commit('common/updatePageTitle', '订单系统')
+    // 还需要保存上级路由地址到store中去
+    this.$store.commit('common/updateHeaderParentPath', '/home/firstpage')
+  },
   activated() {},
   deactivated() {},
   updated() {},
