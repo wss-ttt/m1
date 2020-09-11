ae9f185a708bec82cddcddc20e40430a101176a5
王水胜 <602466011@qq.com>
2020-09-11 16:01:09 +0800

    更新： order.vue 无线滚动

--- src/views/home/order.vue
+++ src/views/home/order.vue
@@ -1,29 +1,65 @@
 <template>
-    <div class="wrapper">
-       order
-   </div>
+  <div class="wrapper">
+    <ul
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="loading"
+      infinite-scroll-distance="0"
+    >
+      <li v-for="(item,index) in list" :key="index">aa{{ item }}</li>
+      <p class="more">加载更多...</p>
+    </ul>
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    props: {},
-    data() {
-      return {
-          
-      }
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    activated() {},
-    deactivated() {},
-    updated() {},
-    destroyed() {},
-    methods: {},
-    filters: {}
-  }
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      loading: false,
+      list: [1, 2, 3, 4, 5, 6]
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    loadMore() {
+      this.loading = true
+      console.log('loadMore')
+      setTimeout(() => {
+        let last = this.list[this.list.length - 1]
+        for (let i = 1; i <= 10; i++) {
+          this.list.push(last + i)
+        }
+        this.loading = false
+      }, 2000)
+    }
+  },
+  filters: {}
+}
 </script>
-<style scoped>
+<style scoped lang="scss">
+.wrapper {
+  // background: #fb3;
+  overflow: auto;
+  height: 100%;
+  ul {
+    li {
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      margin-bottom: 10px;
+      height: 100px;
+    }
+  }
+  .more {
+    text-align: center;
+  }
+}
 </style>
