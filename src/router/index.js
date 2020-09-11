df836fb3b7256cc9c24021cf0ab64a5cdbdb9cb7
王水胜 <602466011@qq.com>
2020-09-11 08:42:15 +0800

    更新： router.js 重定向

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,60 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      // 添加重定向
+      redirect: {
+        name: "firstname"
+      }
     },
     {
-      path: '/index',
-      name: 'index',
-      component: resovle => require(['@/views/index.vue'], resovle)
+      path: "/index",
+      name: "index",
+      component: resovle => require(["@/views/index.vue"], resovle)
     },
     {
-      path: '/home',
-      name: 'home',
-      component: resovle => require(['@/views/home/home.vue'], resovle),
-      children: [{
-        path: 'firstpage',
-        name: 'firstname',
-        component: resovle => require(['@/views/home/firstpage.vue'], resovle),
-      }, {
-        path: 'order',
-        name: 'order',
-        component: resovle => require(['@/views/home/order.vue'], resovle),
-      }, {
-        path: 'shopcar',
-        name: 'shopcar',
-        component: resovle => require(['@/views/home/shopcar.vue'], resovle),
-      }, {
-        path: 'personal',
-        name: 'personal',
-        component: resovle => require(['@/views/home/personal.vue'], resovle),
-      }]
+      path: "/home",
+      name: "home",
+      component: resovle => require(["@/views/home/home.vue"], resovle),
+      children: [
+        {
+          path: "firstpage",
+          name: "firstname",
+          component: resovle => require(["@/views/home/firstpage.vue"], resovle)
+        },
+        {
+          path: "order",
+          name: "order",
+          component: resovle => require(["@/views/home/order.vue"], resovle)
+        },
+        {
+          path: "shopcar",
+          name: "shopcar",
+          component: resovle => require(["@/views/home/shopcar.vue"], resovle)
+        },
+        {
+          path: "personal",
+          name: "personal",
+          component: resovle => require(["@/views/home/personal.vue"], resovle)
+        }
+      ]
     },
     {
-      path: '/firstpage/order_system',
-      name: 'order_system',
-      component: resovle => require(['@/views/firstpage/order_system.vue'], resovle),
+      path: "/firstpage/order_system",
+      name: "order_system",
+      component: resovle =>
+        require(["@/views/firstpage/order_system.vue"], resovle)
     },
     {
-      path: '/firstpage/dealer',
-      name: 'dealer',
-      component: resovle => require(['@/views/firstpage/dealer.vue'], resovle),
+      path: "/firstpage/dealer",
+      name: "dealer",
+      component: resovle => require(["@/views/firstpage/dealer.vue"], resovle)
     }
   ]
-})
+});
