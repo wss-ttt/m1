4919514c6c28fcbcb1f6d3190443abe911e92f57
王水胜 <602466011@qq.com>
2020-09-11 16:10:40 +0800

    更新：shopcar

--- src/views/home/shopcar.vue
+++ src/views/home/shopcar.vue
@@ -1,29 +1,27 @@
 <template>
-    <div class="wrapper">
-       shopcar
-   </div>
+  <div class="shopcar">
+    <div class="box">好好</div>
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    props: {},
-    data() {
-      return {
-          
-      }
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    activated() {},
-    deactivated() {},
-    updated() {},
-    destroyed() {},
-    methods: {},
-    filters: {}
-  }
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
 </script>
 <style scoped>
 </style>
